f15aadb507eef13dcaec6bfbc9367a076a1f6893
zerufael <zerufaelaemero@gmail.com>
2024-10-04 10:18:20 -0700

    edit

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,11 +2,10 @@
 '''Modules of task-5.
 '''
 
+from functools import reduce
+
 
 def sum_list(input_list: list[float]) -> float:
     '''Takes a list of floats and returns their sum as a float
     '''
-    sum: float = 0
-    for num in input_list:
-        sum += num
-    return sum
+    return float(sum(input_list))
