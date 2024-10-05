7487f2545cb2390f1960cdb7981cc5dfc7c1b837
zerufael <zerufaelaemero@gmail.com>
2024-10-05 08:50:41 -0700

    tuple

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''Modules of task-7.
+'''
+
+
+from typing import Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> tuple:
+    '''returns a tuple
+    '''
+    return (k, v*v)
