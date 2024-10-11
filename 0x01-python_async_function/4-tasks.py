3f019ed0caf47a01e7f0c47585f82f8b6ccddc5e
zerufael <zerufaelaemero@gmail.com>
2024-10-11 04:29:15 -0700

    edit

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -11,7 +11,5 @@ task_wait_random = __import__('3-tasks').task_wait_random
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     '''Executes task_wait_random n times.
     '''
-    wait_times = await asyncio.gather(
-        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
-    )
-    return sorted(wait_times)
+    delays = [task_wait_random(max_delay) for _ in range(n)]
+    return sorted(await asyncio.gather(*delays))
