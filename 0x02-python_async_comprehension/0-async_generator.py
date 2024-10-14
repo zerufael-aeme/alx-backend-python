eac8a2da63c96937e8982ff2413e35de7edd79ab
zerufael <zerufaelaemero@gmail.com>
2024-10-14 07:08:11 -0700

    edit

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -11,4 +11,4 @@ async def async_generator() -> Generator[float, None, None]:
     '''
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.random() * 10
+        yield random.uniform(0, 10)
